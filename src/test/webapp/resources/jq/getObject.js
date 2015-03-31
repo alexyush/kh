de261b20a5c0d8c6f01810af03260339d66b831c
alexyush <lelikone93@mail.ru>
2015-03-31 17:58:34 +0300

    delete old files

--- src/test/webapp/resources/jq/getObject.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/kitty/get/1"
-    }).then(function(data) {
-       $('.kitty-id').append(data.id);
-       $('.kitty-name').append(data.name);
-       $('.kitty-message').append(data.message);
-    });
-});
